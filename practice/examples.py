29d3cdaf178b9c162455f0e40c128e1838b4b71b
lakshayrwt <97087675+lakshayrwt@users.noreply.github.com>
2023-04-05 19:45:30 +0530

    used index positions

--- practice/examples.py
+++ practice/examples.py
@@ -38,11 +38,12 @@ b = 3
 print(a + b / 3 - 4 * 12)
 print(((a + b )/ 3 - 4 ) * 12)
 print()
+#         01234567890123
 parrot = "Norwegian Blue"
 print(parrot)
 print(parrot[3])
 print(parrot[13])
-print()
+print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
