0bb82a365b15619b7e4061c888822bb0602a170a
lakshayrwt <97087675+lakshayrwt@users.noreply.github.com>
2023-04-08 19:45:39 +0530

    learned program control flow in python

--- practice/examples.py
+++ practice/examples.py
@@ -235,3 +235,45 @@ flower = "blue violet"
 print('blue'in flower)
 
 
+for i in range (1, 13):
+    print("No.{} squared is {} and cubed is {:4}".format(i,i ** 2, i ** 3))
+    print("*" * 80)
+
+print()
+
+def checkage():
+    name = input("name: ")
+    age = int(input("age: "))
+    print(type(age) )
+    if age < 18:
+       return 'get out of here'
+    else :
+       return "Welcome " + name + " sir"    
+print(checkage())
+
+print()
+
+def checkage(age):
+    if age < 18:
+        return "get out"
+    else:
+        return "welcome"
+print(checkage(19))
+
+name = input("please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
+
+if age >= 18:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
+else:
+    print("Please come back in {0} years".format(18 - age))
+
+if age < 18:
+    print("Please come back in {0} years".format(18 - age))
+elif age == 1000:
+    print("The Almighty, has returned after 1000 years")
+else:
+    print("you are old enough to vote")
+    print("Please put an X in the box")
